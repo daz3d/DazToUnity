911043183d422041427858edd8a5ec03f63fac97
danielbui78 <danielbui78@gmail.com>
2024-07-11 09:04:20 -0400

    2024 update, part 1

--- DazStudioPlugin/version.h
+++ DazStudioPlugin/version.h
@@ -2,9 +2,9 @@
 #include "dzversion.h"
 
 // Version number for dzBridge-Unity
-#define PLUGIN_MAJOR	2023
+#define PLUGIN_MAJOR	2024
 #define PLUGIN_MINOR	1
 #define PLUGIN_REV		1
-#define PLUGIN_BUILD	11
+#define PLUGIN_BUILD	12
 
 #define PLUGIN_VERSION	DZ_MAKE_VERSION( PLUGIN_MAJOR, PLUGIN_MINOR, PLUGIN_REV, PLUGIN_BUILD )
